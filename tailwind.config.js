113250e287c30b8c27f7777f8422e0afadcff17f
RashiSaxena <rashi.s.505@gmail.com>
2022-05-29 13:00:01 -0400

    Modify colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,13 @@ module.exports = {
         black: '#000000',
         white: '#fff',
         primary: {
-          default: '#ff8304', //'#8174ff',
-          darker: /*'#fccf00',*/'#4c2701', //'#211d39',
+          default: '#126f52', //'#ff8304'dark orange, //'#8174ff',
+          darker:  '#034732',/*'#fccf00' brown,*/'#4c2701', //'#211d39',
           translucent: 'rgba(76, 39, 1, 0.5)' //'rgba(33, 29, 57, 0.8)'
         },
         secondary: {
-          default: '#fccf00',//'#a0eec0',
-          darker: '#e3ba00' //'#8ae9c1'
+          default: '#8b728e',  //'#fccf00'yellow ,//'#a0eec0',
+          darker:  '#694873', //'#e3ba00' darker yellow //'#8ae9c1'
         }
       },
       screens: {
