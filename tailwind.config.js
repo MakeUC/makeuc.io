a934ed1619960b560429452e03b935cf60e137cb
RashiSaxena <rashi.s.505@gmail.com>
2022-05-29 14:15:01 -0400

    Fix commenting error

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         white: '#fff',
         primary: {
           default: '#126f52', //'#ff8304'dark orange, //'#8174ff',
-          darker:  '#034732',/*'#fccf00' brown,*/'#4c2701', //'#211d39',
+          darker:  '#034732',/*'#fccf00' brown,*///'#4c2701', //'#211d39',
           translucent: 'rgba(76, 39, 1, 0.5)' //'rgba(33, 29, 57, 0.8)'
         },
         secondary: {
