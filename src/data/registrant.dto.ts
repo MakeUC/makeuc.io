a5cf3f8b6ec37496eb45aead60c611754918d80e
John <john@pocket-e.com>
2022-06-17 02:31:41 -0400

    feat(client): Enabled registration

--- src/data/registrant.dto.ts
+++ src/data/registrant.dto.ts
@@ -1,18 +1,19 @@
-export class RegistrantDTO {
-  readonly fullName: string
-  readonly email: string
-  readonly phone: string
-  readonly school: string
-  readonly country: string
-  readonly degree: string
-  readonly major: string
-  readonly graduation: number
-  readonly hackathonsAttended: string
-  readonly resume?: FileList
-  readonly ethnicity: string
-  readonly gender: string
-  readonly questions?: string
-  readonly agreed: boolean
-  readonly authorized: boolean
-  readonly communication: boolean
-}
+export type RegistrantDTO = {
+  fullName: string;
+  age: number;
+  email: string;
+  phone: string;
+  school: string;
+  country: string;
+  degree: string;
+  major: string;
+  graduation: number;
+  hackathonsAttended: string;
+  resume?: FileList;
+  ethnicity: string;
+  gender: string;
+  questions?: string;
+  agreed: boolean;
+  authorized: boolean;
+  communication: boolean;
+};
